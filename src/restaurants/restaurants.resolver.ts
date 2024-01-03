c4c9e30b3fdec1b7d221db0ad288bbdf27fc57ee
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-01-03 22:09:33 +0900

    2.3 Arguments

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query(() => Restaurant)
-  isPizzaGood(): Restaurant {
-    return {
-      name: '배민',
-    };
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    console.log(veganOnly);
+    return [];
   }
 }
