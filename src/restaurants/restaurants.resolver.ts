5bf945e01ff571a2dd162718aa242d9562349bdc
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-01-03 20:42:23 +0900

    2.2 ObjectType

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,12 @@
 import { Query, Resolver } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
 
-@Resolver()
+@Resolver((of) => Restaurant)
 export class RestaurantResolver {
-  @Query(() => Boolean)
-  isPizzaGood(): boolean {
-    return true;
+  @Query(() => Restaurant)
+  isPizzaGood(): Restaurant {
+    return {
+      name: '배민',
+    };
   }
 }
