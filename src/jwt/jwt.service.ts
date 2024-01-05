a89773d03a204968f872ecbc314a66685b2fa1e3
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-01-06 03:32:54 +0900

    6.4 JWT Module part Two

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { JWT_OPTIONS } from './jwt.constants';
 
 @Injectable()
 export class JwtService {
+  constructor(
+    @Inject(JWT_OPTIONS) private readonly options: JwtModuleOptions,
+  ) {}
   hello() {
     console.log('hello');
   }
