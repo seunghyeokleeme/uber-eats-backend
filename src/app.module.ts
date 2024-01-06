26f5c59fe0517510a45886f7181acb3dd545b385
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-01-06 22:09:20 +0900

    6.8 GraphQL Context

--- src/app.module.ts
+++ src/app.module.ts
@@ -45,6 +45,7 @@ import { JwtMiddleware } from './jwt/jwt.middleware';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: true,
+      context: ({ req }) => ({ user: req['user'] }),
     }),
     UsersModule,
     CommonModule,
@@ -59,7 +60,7 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(JwtMiddleware).forRoutes({
       path: '/graphql',
-      method: RequestMethod.ALL,
+      method: RequestMethod.POST,
     });
   }
 }
